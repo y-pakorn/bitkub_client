72d4b558927b6fe5e5c36279bb7376d34b93b99f
Pakorn Nathong <goddesign11294_2@hotmail.com>
2021-02-05 01:11:18 +0700

    Move files into src

--- bin/bitkub_client.dart
+++ bin/bitkub_client.dart
@@ -1,7 +1,3 @@
-import 'client.dart';
+export 'src/client.dart';
 
-void main(List<String> arguments) async {
-  var client = BitkubClient();
-  var tickers = await client.getTickers();
-  print(tickers.tickerList.first.symbol);
-}
+void main(List<String> arguments) async {}
